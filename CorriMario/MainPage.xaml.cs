9f84b31565dc5f4fc39575c5af5ef70c79748a63
Mario Cosmi <546836@gmail.com>
2013-01-15 16:39:45 +0100

    Prima sottomissione al market

--- CorriMario/MainPage.xaml.cs
+++ CorriMario/MainPage.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using System.Device.Location;
@@ -27,6 +19,7 @@ namespace CorriMario {
 			PhoneApplicationService.Current.State["lastLat"] = "0";
 			PhoneApplicationService.Current.State["lastLong"] = "0";
 			this.DataContext = this;
+			AggiornaMusetto(0);
 		}
 
 		public event PropertyChangedEventHandler PropertyChanged;
@@ -83,8 +76,8 @@ namespace CorriMario {
 
 		private void Button_Tap_1(object sender, GestureEventArgs e) {
 			if (_watcher == null) {
-				_watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.Default);
-				_watcher.MovementThreshold = 20;
+				_watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+				_watcher.MovementThreshold = 25;
 				_watcher.StatusChanged += OnStatusChanged;
 				_watcher.PositionChanged += OnPositionChanged;
 			}
@@ -92,9 +85,13 @@ namespace CorriMario {
 				if (this.Avviato) {
 					_watcher.Stop();
 					this.btnStartStop.Text = "Riparti";
+					PhoneApplicationService phoneAppService = PhoneApplicationService.Current;
+					phoneAppService.UserIdleDetectionMode = IdleDetectionMode.Enabled;
 				} else {
 					_watcher.Start();
 					this.btnStartStop.Text = "Fai una pausa";
+					PhoneApplicationService phoneAppService = PhoneApplicationService.Current;
+					phoneAppService.UserIdleDetectionMode = IdleDetectionMode.Disabled;
 				}
 				this.Avviato = !this.Avviato;
 			} catch (Exception) {
@@ -121,6 +118,7 @@ namespace CorriMario {
 					this.Distanza = totDist.ToString("Hai corso per 0 m.");
 					AggiornaMusetto(totDist);
 				}
+//				this.btnStartStop.Text = string.Format("Ultima lettura {0} {1}", e.Position.Location.Longitude, e.Position.Location.Latitude);
 				PhoneApplicationService.Current.State["lastLong"] = e.Position.Location.Longitude;
 				PhoneApplicationService.Current.State["lastLat"] = e.Position.Location.Latitude;
 			} catch (Exception) {
@@ -129,21 +127,22 @@ namespace CorriMario {
 			}
 		}
 
+		string ImmagineAdatta(double totDist) {
+			return totDist < 3000 ? "level1.jpg" : totDist < 6000 ? "level2.jpg" : "level3.jpg";
+		}
+
+		string ImmagineSfondo(double totDist) {
+			return totDist < 3000 ? "level0.jpg" : totDist < 6000 ? "level1.jpg" : "level2.jpg";
+		}
+
 		void AggiornaMusetto(double totDist) {
 			// dovrebbe essere 5 km per etto di musetto ma io sono buono ...
-			string immagine;
-			if (totDist < 3000) {
-				immagine = "level1.jpg";
-			} else if (totDist < 6000) {
-				immagine = "level2.jpg";
-			} else {
-				immagine = "level3.jpg";
-			}
-			immagine = totDist < 3000 ? "level1.jpg" : totDist < 6000 ? "level2.jpg" : "level3.jpg";
-			BitmapImage bmp = new BitmapImage(new Uri(immagine, UriKind.Relative));
-			this.imgMusetto.Source = bmp;
+			this.imgMusetto.Source = new BitmapImage(new Uri(ImmagineAdatta(totDist), UriKind.Relative));
 			this.Percentuale = totDist < 9000 ? this.imgMusetto.ActualHeight * (totDist % 3000) / 3000 : this.imgMusetto.ActualHeight;
 			this.clipMusetto.Rect = new Rect(0, 0, this.imgMusetto.ActualWidth, this.Percentuale);
+			// Ora aggiorno lo sfondo
+			this.imgMusettoSfondo.Source = new BitmapImage(new Uri(ImmagineSfondo(totDist), UriKind.Relative));
+			this.clipMusettoSfondo.Rect = new Rect(0, this.Percentuale, this.imgMusetto.ActualWidth, this.ContentPanel.ActualHeight);
 		}
 
 		void OnStatusChanged(object sender, GeoPositionStatusChangedEventArgs e) {
--- CorriMario/Properties/AssemblyInfo.cs
+++ CorriMario/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CorriMario")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Corri Mario")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mario Cosmi")]
 [assembly: AssemblyProduct("CorriMario")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright Mario Cosmi©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: NeutralResourcesLanguageAttribute("it")]
